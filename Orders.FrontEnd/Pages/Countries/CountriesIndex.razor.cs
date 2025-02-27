ae6f49eab6ebba3d522c841055eb8efd731e449d
Ernesto Gonzalez Salgado <cegslevono1994@gmail.com>
2025-02-26 23:58:34 -0500

    Crud completed of Countries

--- Orders.FrontEnd/Pages/Countries/CountriesIndex.razor.cs
+++ Orders.FrontEnd/Pages/Countries/CountriesIndex.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
 using Orders.FrontEnd.Repositories;
 using Orders.Shared.Entities;
 
@@ -7,13 +8,65 @@ namespace Orders.FrontEnd.Pages.Countries
     public partial class CountriesIndex
     {
         [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService sweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
         public List<Country>? Countries { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            await base.OnInitializedAsync();
+            await LoadAsync();
+        }
+
+        private async Task LoadAsync()
+        {
             var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
+            if (responseHttp.Error)
+            {
+                var messageError = await responseHttp.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                return;
+            }
             Countries = responseHttp.Response;
+        }
+
+        private async Task DeleteAsync(Country country)
+        {
+            var result = await sweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmacion",
+                Text = $"¿Estas seguro que quieres borrar el pais: {country.Name}?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true,
+            });
+
+            var confirm = string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+            var responseHttp = await Repository.DeleteAsync<Country>($"api/countries/{country.Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("/");
+                }
+                else
+                {
+                    var messageError = await responseHttp.GetErrorMessageAsync();
+                    await sweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                }
+                return;
+            }
+            await LoadAsync();
+            var toast = sweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000,
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro borrado con exito");
         }
     }
 }
