a8d8093949f1a1fd65f239827d76e39417e60e30
Ernesto Gonzalez Salgado <cegslevono1994@gmail.com>
2025-02-28 23:02:00 -0500

    Interfaces success of Category in the frontend

---
+++ Orders.FrontEnd/Pages/Categories/CategoriesIndex.razor.cs
@@ -0,0 +1,72 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.FrontEnd.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.FrontEnd.Pages.Categories
+{
+    public partial class CategoriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService sweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+        public List<Category>? Categories { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadAsync();
+        }
+
+        private async Task LoadAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Category>>("api/categories");
+            if (responseHttp.Error)
+            {
+                var messageError = await responseHttp.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                return;
+            }
+            Categories = responseHttp.Response;
+        }
+
+        private async Task DeleteAsync(Category category)
+        {
+            var result = await sweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmacion",
+                Text = $"¿Estas seguro que quieres borrar la categoria: {category.Name}?",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true,
+            });
+
+            var confirm = string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+            var responseHttp = await Repository.DeleteAsync<Category>($"api/categories/{category.Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("/");
+                }
+                else
+                {
+                    var messageError = await responseHttp.GetErrorMessageAsync();
+                    await sweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                }
+                return;
+            }
+            await LoadAsync();
+            var toast = sweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000,
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro borrado con exito");
+        }
+    }
+}
---
+++ Orders.FrontEnd/Pages/Categories/CategoryEdit.razor.cs
@@ -0,0 +1,66 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.FrontEnd.Pages.Countries;
+using Orders.FrontEnd.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.FrontEnd.Pages.Categories
+{
+    public partial class CategoryEdit
+    {
+        private Category? category;
+        private CategoryForm? categoryForm;
+        [Inject] private IRepository repository { get; set; } = null!;
+        [Inject] private SweetAlertService sweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+        [Parameter] public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var responseHttp = await repository.GetAsync<Category>($"api/categories/{Id}");
+
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("/categories");
+                }
+                else
+                {
+                    var messageError = await responseHttp.GetErrorMessageAsync();
+                    await sweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+                }
+            }
+            else
+            {
+                category = responseHttp.Response;
+            }
+        }
+
+        private async Task EditAsync()
+        {
+            var responseHttp = await repository.PutAsync("/api/categories", category);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await sweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+            var toast = sweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000,
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Cambios guardados con exito");
+        }
+
+        private void Return()
+        {
+            categoryForm!.FormPostedSuccessfully = true;
+            navigationManager.NavigateTo("/categories");
+        }
+    }
+}
