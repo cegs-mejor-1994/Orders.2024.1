b2de2f927731dedd6fa63378c75a2a5d55d00a2b
Ernesto Gonzalez Salgado <cegslevono1994@gmail.com>
2025-03-02 16:08:40 -0500

    States: repo, uow and controlles, and city controller

---
+++ Orders.BackEnd/Controllers/CitiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.BackEnd.UnitsOfWork.Interfaces;
+using Orders.Shared.Entities;
+
+namespace Orders.BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : GenericController<City>
+    {
+        public CitiesController(IGenericUnitOfWork<City> unitOfWork) : base(unitOfWork)
+        {
+            
+        }
+    }
+}
---
+++ Orders.BackEnd/Repositories/Implementations/StatesRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.BackEnd.Data;
+using Orders.BackEnd.Repositories.Interfaces;
+using Orders.Shared.Entities;
+using Orders.Shared.Responses;
+
+namespace Orders.BackEnd.Repositories.Implementations
+{
+    public class StatesRepository : GenericRepository<State>, IStatesRepository
+    {
+        private readonly DataContext _context;
+
+        public StatesRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+        public override async Task<ActionResponse<State>> GetAsync(int id)
+        {
+            var state = await _context.States.Include(c => c.Cities).FirstOrDefaultAsync(c => c.Id == id);
+
+            if (state == null)
+            {
+                return new ActionResponse<State>
+                {
+                    WasSuccess = false,
+                    Message = "El Estado no existe"
+                };
+            }
+
+            return new ActionResponse<State>
+            {
+                WasSuccess = true,
+                Result = state
+            };
+        }
+
+        public override async Task<ActionResponse<IEnumerable<State>>> GetAsync()
+        {
+            var states = await _context.States.Include(c => c.Cities).ToListAsync();
+
+            return new ActionResponse<IEnumerable<State>>
+            {
+                WasSuccess = true,
+                Result = states
+            };
+        }
+    }
+}
