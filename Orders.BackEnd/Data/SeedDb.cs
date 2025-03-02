05d37383a5c5bace08968d424d2f77c9ba530639
Ernesto Gonzalez Salgado <cegslevono1994@gmail.com>
2025-03-01 21:04:57 -0500

    Seed the database

---
+++ Orders.BackEnd/Data/SeedDb.cs
@@ -0,0 +1,43 @@
+﻿using Orders.Shared.Entities;
+
+namespace Orders.BackEnd.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+            await CheckCategoriesAsync();
+        }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_context.Countries.Any())
+            {
+                _context.Countries.Add(new Country { Name = "Ecuador" });
+                _context.Countries.Add(new Country { Name = "Mexico" });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Autos" });
+                _context.Categories.Add(new Category { Name = "Herramientas" });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
