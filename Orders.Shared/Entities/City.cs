40717ef12a79f2bfcad81aca9c89f6584cb8e707
Ernesto Gonzalez Salgado <cegslevono1994@gmail.com>
2025-03-07 23:02:05 -0500

    Add user Entity

--- Orders.Shared/Entities/City.cs
+++ Orders.Shared/Entities/City.cs
@@ -18,5 +18,7 @@ namespace Orders.Shared.Entities
 
         public int StateId { get; set; }
         public State? State { get; set; }
+
+        public ICollection<User>? Users { get; set; }
     }
 }
